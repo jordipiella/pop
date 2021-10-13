71c5502d5d93505eb2cebde89e1835ef80bb22af
jordipiella <jordipiella@gmail.com>
2021-10-13 22:22:55 +0200

    feat: search fn favorite component and test

--- src/app/modules/favorites/favorites.component.ts
+++ src/app/modules/favorites/favorites.component.ts
@@ -1,9 +1,10 @@
-import { Component, AfterViewInit, ChangeDetectorRef, OnDestroy } from '@angular/core';
-import { delay, startWith, tap } from 'rxjs/operators';
+import { Component, AfterViewInit, OnDestroy } from '@angular/core';
+import { debounceTime, delay, distinctUntilChanged, startWith, tap } from 'rxjs/operators';
 import { FormBuilder, FormControl } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { FavoriteModel } from '@core';
 import { FavoritesFacade } from './services/favorite.facade';
+import { normalizeDiacritics } from 'src/app/utils/normalize-diacritics';
 
 
 @Component({
@@ -14,6 +15,7 @@ import { FavoritesFacade } from './services/favorite.facade';
 export class FavoritesComponent implements AfterViewInit, OnDestroy {
 
   favorites: FavoriteModel[] = [];
+  allFavorites: FavoriteModel[] = [];
   searchForm: FormControl = this.fb.control('');
   subscriptions: Subscription[] = [];
 
@@ -25,6 +27,7 @@ export class FavoritesComponent implements AfterViewInit, OnDestroy {
 
   ngAfterViewInit(): void {
     this.getFavorites();
+    this.searchSub();
   }
 
   ngOnDestroy(): void {
@@ -37,29 +40,42 @@ export class FavoritesComponent implements AfterViewInit, OnDestroy {
         startWith([]),
         delay(0),
         tap((favorites: FavoriteModel[]) => this.setFavorites(favorites)),
+        tap(() => this.setSearch(this.searchForm.value)),
       ).subscribe();
   }
 
   setFavorites(favorites: FavoriteModel[] = []): void {
     this.favorites = [...favorites];
+    this.allFavorites = [...favorites];
   }
-  // TODO:
-  // searchSub(): void {
-  //   const searchSub: Subscription = this.searchForm.valueChanges
-  //     .pipe(
-  //       filter((value: string) => value.length > 2 || !value),
-  //       debounceTime(500),
-  //       distinctUntilChanged(),
-  //       tap((value: string) => this.setSearch(value)),
-  //     ).subscribe();
-  //   this.subscriptions.push(searchSub);
-  // }
 
-  // setSearch(value: string): void {
-  //   if (!value) {
-  //     return;
-  //   }
-  // }
+  searchSub(): void {
+    const searchSub: Subscription = this.searchForm.valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged(),
+        tap((value: string) => this.setSearch(value)),
+      ).subscribe();
+    this.subscriptions.push(searchSub);
+  }
+
+  setSearch(value: string): void {
+    if (!value) {
+      this.setFavorites(this.allFavorites);
+      return;
+    }
+    this.favorites = this.filterFavorites(this.allFavorites, value);
+  }
+
+  // TODO: Refactor to generic fn to filter arrays
+  filterFavorites(favorites: FavoriteModel[] = [], value: string = ''): FavoriteModel[] {
+    const normValue: string = normalizeDiacritics(value);
+    const regex: RegExp = new RegExp(normValue, 'gi');
+    return favorites.filter((x) => {
+      const normTitle: string = (x?.title )? normalizeDiacritics(x.title) : '';
+      return normTitle.match(regex);
+    });
+  }
 
   removeFavorite(favorite: FavoriteModel): void {
     this.favoritesFacade.removeFavorite(favorite);
