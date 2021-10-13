3f0325945da689166c61243841a8638c88548c16
jordipiella <jordipiella@gmail.com>
2021-10-13 09:49:13 +0200

    feat: favorites service

---
+++ src/app/core/services/favorites/mocks/favorites-mock.model.ts
@@ -0,0 +1,6 @@
+import { FavoriteModel } from "../models/favorite.model";
+
+export const favoriteMockModel: FavoriteModel = {
+  title: 'Cascos',
+  image: 'https://frontend-tech-test-data.s3-eu-west-1.amazonaws.com/img/headphones.jpg'
+};
---
+++ src/app/core/services/favorites/models/favorite.model.ts
@@ -0,0 +1,5 @@
+
+export class FavoriteModel {
+  title: string = '';
+  image: string = '';
+}
