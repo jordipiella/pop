925b95d7428296cdf4b8178a9241b15376e7098b
jordipiella <jordipiella@gmail.com>
2021-10-13 20:25:08 +0200

    fix: core index

--- src/app/core/index.ts
+++ src/app/core/index.ts
@@ -6,10 +6,13 @@ export * from './services/app.facade';
 
 // Components
 export * from './header/header.component';
-export * from './modal/modal.component';
+export * from '../shared/modal/modal.component';
 
 // Models
 export * from './services/favorites/models/favorite.model';
 
 // Mocks
 export * from './services/favorites/mocks/favorites-mock.model';
+export * from './mocks/mock-component';
+export * from './mocks/view-container-ref.mock';
+export * from './mocks/component-ref.mock';
