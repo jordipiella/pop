5cc9f49ac785d10e626838853349943319d338b6
jordipiella <jordipiella@gmail.com>
2021-10-13 10:44:44 +0200

    test: app facade

--- src/app/core/services/app.facade.spec.ts
+++ src/app/core/services/app.facade.spec.ts
@@ -4,6 +4,8 @@ import { Store, StoreModule } from '@ngrx/store';
 import { AppFacade } from './app.facade';
 import { IFavoritesState } from '../state/favorites/favorites.reducer';
 import * as fromFavorites from '../state/favorites/favorites.reducer';
+import { favoriteMockModel } from './favorites/mocks/favorites-mock.model';
+import { addFavorite, removeFavorite } from '../state/favorites/favorites.actions';
 
 describe('AppFacade', () => {
   let service: AppFacade;
@@ -23,9 +25,19 @@ describe('AppFacade', () => {
     store = TestBed.inject(Store);
   });
 
-  describe('#()', () => {
-    it('should call ', () => {
+  describe('#addFavorite()', () => {
+    it('should call should call store.dispatch with addFavorite', () => {
+      spyOn(store, 'dispatch');
+      service.addFavorite(favoriteMockModel);
+      expect(store.dispatch).toHaveBeenCalledWith(addFavorite({ data: [ favoriteMockModel ] }));
+    });
+  });
 
+  describe('#removeFavorite()', () => {
+    it('should call store.dispatch with removeFavorite', () => {
+      spyOn(store, 'dispatch');
+      service.removeFavorite(favoriteMockModel);
+      expect(store.dispatch).toHaveBeenCalledWith(removeFavorite({ data: [ favoriteMockModel ]}));
     });
   });
 
