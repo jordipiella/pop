0c14a43f1424bb1283f095e8509d55c7d0afd799
jordipiella <jordipiella@gmail.com>
2021-10-14 19:00:51 +0200

    feat: add error alert to error handler

--- src/app/core/interceptors/error.interceptor.ts
+++ src/app/core/interceptors/error.interceptor.ts
@@ -2,15 +2,23 @@ import { Injectable } from '@angular/core';
 import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { AppFacade } from '../services/app.facade';
+import { TranslateService } from '@ngx-translate/core';
 
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(
+    private appFacade: AppFacade,
+    private translate: TranslateService
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
+        const title: string = this.translate.instant('error.title');
+        const text: string = error?.message ? error.message : this.translate.instant('error.text');
+        this.appFacade.errorAlert(title, text);
         return throwError(error);
       })
     )
