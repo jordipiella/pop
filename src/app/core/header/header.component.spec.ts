50c5cd7ee69f514fac3a01e1f9ef0137e6dd1d72
jordipiella <jordipiella@gmail.com>
2021-10-15 16:45:53 +0200

    refactor: header component

--- src/app/core/header/header.component.spec.ts
+++ src/app/core/header/header.component.spec.ts
@@ -2,19 +2,36 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header.component';
 import { MockComponent } from '../mocks/mock-component';
+import { AppFacade } from '../services/app.facade';
+import { provideMockStore } from '@ngrx/store/testing';
+import { TranslateService, TranslateModule } from '@ngx-translate/core';
 
-describe('DropDownComponent', () => {
+const initialState: unknown = {
+  data: []
+};
+
+describe('HeaderComponnet', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
+  let appFacade: AppFacade;
+  let translate: TranslateService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(() => {
+    TestBed.configureTestingModule({
       declarations: [
         HeaderComponent,
         MockComponent({ selector: 'svg-icon' })
+      ],
+      providers: [
+        provideMockStore({ initialState: { favorites: initialState } })
+      ],
+      imports: [
+        TranslateModule.forRoot()
       ]
     })
     .compileComponents();
+    appFacade = TestBed.inject(AppFacade);
+    translate = TestBed.inject(TranslateService);
   });
 
   beforeEach(() => {
@@ -23,11 +40,19 @@ describe('DropDownComponent', () => {
     fixture.detectChanges();
   });
 
-  describe('#clickEvent', () => {
-    it('should emit event with string', () => {
-      spyOn(component.eventClick, 'emit');
-      component.clickEvent('star');
-      expect(component.eventClick.emit).toHaveBeenCalledWith('star')
+  describe('#ngOnInit', () => {
+    it('should call translate.instant withheader.title', () => {
+      spyOn(translate, 'instant');
+      component.ngOnInit();
+      expect(translate.instant).toHaveBeenCalledWith('header.title')
+    })
+  })
+
+  describe('#openFavoritesModal', () => {
+    it('should call appFacade.openFavoritesModal', () => {
+      spyOn(appFacade, 'openFavoritesModal');
+      component.openFavoriteModal();
+      expect(appFacade.openFavoritesModal).toHaveBeenCalled()
     })
   })
 
