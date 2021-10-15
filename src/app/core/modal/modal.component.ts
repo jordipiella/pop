d9fd738ecf7b0b76e56a8e0ff4d5b692efb00f90
jordipiella <jordipiella@gmail.com>
2021-10-15 16:55:40 +0200

    refactor: modal component

--- src/app/core/modal/modal.component.ts
+++ src/app/core/modal/modal.component.ts
@@ -1,39 +1,70 @@
 import {
   AfterViewInit,
   Component,
-  EventEmitter,
   ViewChild,
   ViewContainerRef
 } from '@angular/core';
 import { modalAnimation } from '../../shared/animations/animations.constants';
+import { tap } from 'rxjs/operators';
+import { OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AppFacade } from '../services/app.facade';
 
 @Component({
 
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss'],
-  animations: [ modalAnimation ],
-  // eslint-disable-next-line @angular-eslint/no-host-metadata-property
-  host: { '[@modal]': 'in' }
+  animations: [ modalAnimation ]
 })
-export class ModalComponent implements AfterViewInit {
+export class ModalComponent implements AfterViewInit, OnDestroy {
 
-  clickClose: EventEmitter<boolean> = new EventEmitter<boolean>();
-  afterViewInit: EventEmitter<boolean> = new EventEmitter<boolean>();
   @ViewChild('content', { read: ViewContainerRef }) content!: ViewContainerRef;
 
-  visible: boolean = true;
+  isVisible: boolean = false;
+  subscriptions: Subscription[] = [];
 
   constructor(
+    private appFacade: AppFacade
   ) { }
 
   ngAfterViewInit(): void {
-    this.afterViewInit.emit(true);
+    this.isOpenSubs();
+  }
+
+  ngOnDestroy(): void {
+    this.subscriptions.forEach((sub: Subscription) => sub.unsubscribe());
+  }
+
+  isOpenSubs(): void {
+    this.appFacade.isOpen$.pipe(
+      tap((isOpen: boolean) => this.setIsVisible(isOpen)),
+      tap((isOpen: boolean) => this.modalBehaviour(isOpen, this.content))
+    ).subscribe();
+  }
+
+  modalBehaviour(isOpen: boolean, ref: ViewContainerRef): void {
+    if (!isOpen) {
+      return;
+    }
+    // Wait for content
+    setTimeout(() => {
+      this.modalOpened(ref);
+    }, 100);
+  }
+
+  modalOpened(content: ViewContainerRef | null): void {
+    if (content) {
+      this.appFacade.modalOpened(content);
+    }
+  }
+
+  setIsVisible(value: boolean): void {
+    this.isVisible = (value) ? true : false;
   }
 
   close(): void {
-    this.visible = false;
-    this.clickClose.emit(true);
+    this.appFacade.closeModal();
   }
 
 }
