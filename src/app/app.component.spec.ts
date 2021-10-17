d0ee577a15d3d812e81f9c363261b5649fa2cf45
jordipiella <jordipiella@gmail.com>
2021-10-17 11:55:18 +0200

    fix: test spec

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
-import { TranslateModule, TranslateService } from '@ngx-translate/core';
 import { MockComponent } from './core/mocks/mock-component';
 import { ModalService } from './core/services/modal/modal.service';
 import { provideMockStore } from '@ngrx/store/testing';
@@ -15,13 +14,11 @@ describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
   let modalService: ModalService;
-  let translate: TranslateService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         RouterTestingModule,
-        TranslateModule.forRoot()
       ],
       declarations: [
         AppComponent,
@@ -39,18 +36,7 @@ describe('AppComponent', () => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
     modalService = TestBed.inject(ModalService);
-    translate = TestBed.inject(TranslateService);
     fixture.detectChanges();
-  });
-
-  describe('#ngOnInit', () => {
-    it('should call translate setDefaultLang and instant with en and header.title', () => {
-      spyOn(translate, 'setDefaultLang');
-      spyOn(translate, 'instant');
-      component.ngOnInit();
-      expect(translate.setDefaultLang).toHaveBeenCalledWith('en');
-      expect(translate.instant).toHaveBeenCalledWith('header.title');
-    });
   });
 
 });
