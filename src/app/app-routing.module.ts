794c1554c295f24d7ab03a416790bffe40a11cfe
jordipiella <jordipiella@gmail.com>
2021-10-17 11:49:11 +0200

    feat: clean app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { TranslateResolver } from './core/resolvers/translate.resolver';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./pages/home/home.module').then( m => m.HomeModule)
+    resolve: {
+      routeResolver: TranslateResolver
+    },
+    children: [
+      {
+        path: '',
+        loadChildren: () => import('./pages/home/home.module').then( m => m.HomeModule)
+      }
+
+    ]
   },
   {
     path: '**',
